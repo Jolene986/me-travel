0dd57b80678fdff7620ef6b705192dcde4d079cb
Jolene986 <jojovanovic86@gmail.com>
2020-03-02 14:00:19 +0100

    single destination and advice css fixed

--- src/components/StyledHero.js
+++ src/components/StyledHero.js
@@ -5,6 +5,7 @@ const styledHero = styled.header`
   min-height: 80vh;
   background: url(${props => (props.image ? props.image : defaultImg)})
     center/cover no-repeat;
+    width:100%;
   display: flex;
   align-items: center;
   justify-content: center;
