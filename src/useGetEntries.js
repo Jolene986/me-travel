14b92415da29907d931c14babbb75bd05a413027
Jolene <43065205+Jolene986@users.noreply.github.com>
2020-03-17 15:18:53 +0100

    cut console log

--- src/useGetEntries.js
+++ src/useGetEntries.js
@@ -4,12 +4,13 @@ import Client from './Contentful';
 const useGetEntries = (contentType, setContent, formatData) => {
   useEffect(() => {
     Client.getEntries({ content_type: contentType })
-      .then(response => { console.log(response.items)
+      .then(response => { 
+      //format data
         return setContent(formatData(response.items));
       })
       .catch(console.error);
 
-    //format data
+    
   }, [contentType, setContent, formatData]);
 };
 
